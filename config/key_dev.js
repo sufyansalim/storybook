100603dd9997dee7a05663ac50aa8a5411c22c2a
Sufyan Saleem <40366969+sufyansalim@users.noreply.github.com>
2020-06-24 11:41:35 +0500

    version 1.0.0aplha done

--- config/key_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-    mongoURI: "mongodb+srv://supi:Pz52BoeLZC8fnKxW@storybooks-wh026.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    googleClientID: "149863973013-ugdfambhkpt1o8k2scg8jpcdiae0nl80.apps.googleusercontent.com",
-    googleClientSecret: "3Gpzk4oVOFszXUXxlLEZJIrH"
-}
-
